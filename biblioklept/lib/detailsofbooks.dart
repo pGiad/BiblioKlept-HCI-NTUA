3e5a8730a222acc4f618045ed449832baed1144b
Panagiotis Giadikiaroglou <62249158+pGiad@users.noreply.github.com>
2023-02-24 03:23:18 +0200

    DetailsOfBooks and SelectedBookForTrade pages done

--- biblioklept/lib/detailsofbooks.dart
+++ biblioklept/lib/detailsofbooks.dart
@@ -1,10 +1,6 @@
 import 'package:biblioklept/selectbookfortrade.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const BiblioKlept());
-}
-
 class BiblioKlept extends StatelessWidget {
   const BiblioKlept({super.key});
 
@@ -41,7 +37,8 @@ class BookDetailsPage extends StatelessWidget {
   final String address;
   final String email;
 
-  BookDetailsPage({
+  const BookDetailsPage({
+    super.key,
     required this.title,
     required this.username,
     required this.author,
@@ -58,120 +55,211 @@ class BookDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("$title from $username"),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Title: $title',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 8),
-                Divider(thickness: 2, height: 20),
-                Text(
-                  'Author: $author',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Divider(thickness: 2, height: 20),
-                Text(
-                  'Publisher: $publisher',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Divider(thickness: 2, height: 20),
-                Text(
-                  'Summary: $summary',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Divider(thickness: 2, height: 20),
-                Text(
-                  'Number of pages: $pages',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Divider(thickness: 2, height: 20),
-                Text(
-                  'Category: $category',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Divider(thickness: 2, height: 20),
-                Text(
-                  'Condition: $condition',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Divider(thickness: 2, height: 20),
-                Text(
-                  'Year of purchase: $year',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Divider(thickness: 2, height: 20),
-                Text(
-                  'Owner\'s address: $address',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Divider(thickness: 2, height: 20),
-                Text(
-                  'Owner\'s email: $email',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
+        appBar: AppBar(
+          toolbarHeight: 100,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          bottomOpacity: 0,
+          leading: const BackButton(
+            color: Color.fromARGB(255, 112, 4, 80),
           ),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => SelectBookforTrade()),
-                    );
-                  },
-                  child: Text('Select the book you want to trade for'),
-                ),
-                SizedBox(height: 30),
-              ],
-            ),
+          centerTitle: true,
+          title: Text(
+            "$title\nfrom $username",
+            style: const TextStyle(
+                color: Color.fromARGB(255, 112, 4, 80),
+                fontSize: 32,
+                fontFamily: 'Langar-Regular'),
+            textAlign: TextAlign.center,
           ),
-        ],
-      ),
-    );
+        ),
+        body: NotificationListener<OverscrollIndicatorNotification>(
+            onNotification: (overScroll) {
+              overScroll.disallowIndicator();
+              return true;
+            },
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                      padding: const EdgeInsets.all(16),
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 60.0, top: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Title: $title',
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            const Divider(thickness: 2, height: 24),
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(
+                                    fontSize: 16, color: Colors.black),
+                                children: <TextSpan>[
+                                  const TextSpan(
+                                      text: 'Author: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  TextSpan(text: author)
+                                ],
+                              ),
+                            ),
+                            const Divider(thickness: 2, height: 28),
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(
+                                    fontSize: 16, color: Colors.black),
+                                children: <TextSpan>[
+                                  const TextSpan(
+                                      text: 'Publisher: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  TextSpan(text: publisher)
+                                ],
+                              ),
+                            ),
+                            const Divider(thickness: 2, height: 28),
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(
+                                    fontSize: 16, color: Colors.black),
+                                children: <TextSpan>[
+                                  const TextSpan(
+                                      text: 'Summary: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  TextSpan(text: summary)
+                                ],
+                              ),
+                              textAlign: TextAlign.justify,
+                            ),
+                            const Divider(thickness: 2, height: 28),
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(
+                                    fontSize: 16, color: Colors.black),
+                                children: <TextSpan>[
+                                  const TextSpan(
+                                      text: 'Number of Pages: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  TextSpan(text: pages.toString())
+                                ],
+                              ),
+                            ),
+                            const Divider(thickness: 2, height: 28),
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(
+                                    fontSize: 16, color: Colors.black),
+                                children: <TextSpan>[
+                                  const TextSpan(
+                                      text: 'Category: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  TextSpan(text: category)
+                                ],
+                              ),
+                            ),
+                            const Divider(thickness: 2, height: 28),
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(
+                                    fontSize: 16, color: Colors.black),
+                                children: <TextSpan>[
+                                  const TextSpan(
+                                      text: 'Condition: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  TextSpan(text: condition)
+                                ],
+                              ),
+                            ),
+                            const Divider(thickness: 2, height: 28),
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(
+                                    fontSize: 16, color: Colors.black),
+                                children: <TextSpan>[
+                                  const TextSpan(
+                                      text: 'Year of Purchase: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  TextSpan(text: year.toString())
+                                ],
+                              ),
+                            ),
+                            const Divider(thickness: 2, height: 28),
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(
+                                    fontSize: 16, color: Colors.black),
+                                children: <TextSpan>[
+                                  const TextSpan(
+                                      text: 'Owner\'s Address: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  TextSpan(text: address)
+                                ],
+                              ),
+                            ),
+                            const Divider(thickness: 2, height: 28),
+                            RichText(
+                              text: TextSpan(
+                                style: const TextStyle(
+                                    fontSize: 16, color: Colors.black),
+                                children: <TextSpan>[
+                                  const TextSpan(
+                                      text: 'Owner\'s email: ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  TextSpan(text: email)
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SelectBookforTrade()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color.fromARGB(
+                                  255, 112, 4, 80), // sets the background color
+                              foregroundColor:
+                                  Colors.white, // sets the text color
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                              fixedSize: const Size(200, 40)),
+                          child: const Text(
+                            'Find a Book to Give',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                    ],
+                  ),
+                ],
+              ),
+            )));
   }
 }
--- biblioklept/lib/selectbookfortrade.dart
+++ biblioklept/lib/selectbookfortrade.dart
@@ -1,147 +1,158 @@
 import 'package:flutter/material.dart';
 import "package:biblioklept/contactdetailsfortrade.dart";
 
-void main() {
-  runApp(const BiblioKlept());
-}
-
-class BiblioKlept extends StatelessWidget {
-  const BiblioKlept({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'BiblioKlept',
-      home: SelectBookforTrade(),
-    );
-  }
-}
-
 class SelectBookforTrade extends StatefulWidget {
+  const SelectBookforTrade({super.key});
+
   @override
   _SelectBookforTradeState createState() => _SelectBookforTradeState();
 }
 
 class _SelectBookforTradeState extends State<SelectBookforTrade> {
-  List<String> _bookOptions = [
+  final List<String> _bookOptions = [
     'Option 1',
     'Option 2',
     'Option 3',
     'Option 4',
     'Option 5',
+    'Option 6',
+    'Option 7',
+    'Option 8',
+    'Option 9',
+    'Option 10',
+    'Option 11',
+    'Option 12',
+    'Option 13',
+    'Option 14',
+    'Option 15',
   ];
-  List<bool> _isSelected = List.generate(5, (index) => false);
+  final List<bool> _isSelected = List.generate(15, (index) => false);
+
+  int _selectedIndex = -1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('My Books'),
-          ],
+        appBar: AppBar(
+          toolbarHeight: 100,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          bottomOpacity: 0,
+          title: const Text(
+            'My Books',
+            style: TextStyle(
+                color: Color.fromARGB(255, 112, 4, 80),
+                fontSize: 32,
+                fontFamily: 'Langar-Regular'),
+          ),
+          centerTitle: true,
+          leading: const BackButton(
+            color: Color.fromARGB(255, 112, 4, 80),
+          ),
         ),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 16),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Text(
-              'Select your book:',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-              ),
-            ),
-          ),
-          SizedBox(height: 16),
-          Expanded(
-            child: ListView.separated(
-              separatorBuilder: (BuildContext context, int index) {
-                return Divider(
-                  height: 1,
-                  thickness: 1,
-                  color: Colors.grey[300],
-                  indent: 16,
-                  endIndent: 16,
-                );
-              },
-              itemCount: _bookOptions.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          _bookOptions[index],
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      ),
-                      Container(
-                        width: 24,
-                        height: 24,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: _isSelected[index]
-                              ? Colors.blue
-                              : Colors.grey[300],
-                        ),
-                        child: IconButton(
-                          padding: EdgeInsets.zero,
-                          icon: Icon(
-                            Icons.check,
-                            color: Colors.white,
-                            size: 16,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              for (int i = 0; i < _isSelected.length; i++) {
-                                _isSelected[i] = i == index;
-                              }
-                            });
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-          SizedBox(height: 16),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Center(
-              child: MaterialButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ContactFormPage(),
-                    ),
-                  );
-                },
-                color: Colors.blue,
-                textColor: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    'Continue',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
+        body: NotificationListener<OverscrollIndicatorNotification>(
+          onNotification: (overScroll) {
+            overScroll.disallowIndicator();
+            return true;
+          },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 16),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  'Choose a Book to Trade:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
                   ),
                 ),
               ),
-            ),
+              const SizedBox(height: 16),
+              Expanded(
+                child: ListView.separated(
+                  separatorBuilder: (BuildContext context, int index) {
+                    return Divider(
+                      height: 1,
+                      thickness: 1,
+                      color: Colors.grey[300],
+                      indent: 16,
+                      endIndent: 16,
+                    );
+                  },
+                  itemCount: _bookOptions.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _selectedIndex = index;
+                        });
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 14, horizontal: 16),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                _bookOptions[index],
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                            ),
+                            Container(
+                              width: 24,
+                              height: 24,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: _selectedIndex == index
+                                    ? const Color.fromARGB(255, 112, 4, 80)
+                                    : Colors.grey[300],
+                              ),
+                              child: _selectedIndex == index
+                                  ? const Icon(
+                                      Icons.check,
+                                      color: Colors.white,
+                                      size: 16,
+                                    )
+                                  : null,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: _selectedIndex != -1
+                        ? () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ContactFormPage(),
+                              ),
+                            );
+                          }
+                        : null,
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        backgroundColor: const Color.fromARGB(255, 112, 4, 80),
+                        foregroundColor: Colors.white,
+                        fixedSize: const Size(200, 40)),
+                    child:
+                        const Text('Continue', style: TextStyle(fontSize: 18)),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
